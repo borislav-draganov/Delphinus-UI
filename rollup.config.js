3767fcfb523a43cd9cfa34d1c4dd94ef91a9effe
Bobo <bobodraganov@gmail.com>
2020-07-19 16:50:55 +0300

    Removed sirv-cli in favor of rollup-plugin-serve; Had to use beta to have the historyApiFallback working

--- rollup.config.js
+++ rollup.config.js
@@ -7,9 +7,10 @@ import { terser } from 'rollup-plugin-terser';
 import replace from 'rollup-plugin-replace';
 import gzipPlugin from 'rollup-plugin-gzip';
 import copy from 'rollup-plugin-copy'
-import sass from 'node-sass'
+import sass from 'node-sass';
 import autoPreprocess from 'svelte-preprocess'
 import typescript from '@rollup/plugin-typescript';
+import serve from 'rollup-plugin-serve';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -23,21 +24,13 @@ export default {
     },
     plugins: [
         svelte({
-            // enable run-time checks when not in production
             dev: !production,
-            // we'll extract any component CSS out into
-            // a separate file — better for performance
             css: css => {
                 css.write('public/build/bundle.css');
             },
             preprocess: autoPreprocess(),
         }),
 
-        // If you have external dependencies installed from
-        // npm, you'll most likely need these plugins. In
-        // some cases you'll need additional configuration —
-        // consult the documentation for details:
-        // https://github.com/rollup/rollup-plugin-commonjs
         resolve({
             browser: true,
             dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
@@ -78,16 +71,14 @@ export default {
             // ]
         }),
 
-        // In dev mode, call `npm run start` once
-        // the bundle has been generated
-        !production && serve(),
+        !production && serve({
+            contentBase: 'public',
+            port: 5000,
+            historyApiFallback: true,
+        }),
 
-        // Watch the `public` directory and refresh the
-        // browser on changes when not in production
         !production && livereload('public'),
 
-        // If we're building for production (npm run build
-        // instead of npm run dev), minify
         production && terser(),
         
         production && copy({
@@ -100,20 +91,3 @@ export default {
         clearScreen: false
     }
 };
-
-function serve() {
-    let started = false;
-
-    return {
-        writeBundle() {
-            if (!started) {
-                started = true;
-
-                require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-                    stdio: ['ignore', 'inherit', 'inherit'],
-                    shell: true
-                });
-            }
-        }
-    };
-}
